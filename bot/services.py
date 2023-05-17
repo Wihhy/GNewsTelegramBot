a51f107b49ad2ec1e1ff60f12139d4777a7a236f
Wihhy <andre69tkachenko@ukr.net>
2023-05-18 02:27:48 +0300

    Search now works

--- bot/services.py
+++ bot/services.py
@@ -11,32 +11,36 @@ GNEWS_BASE_URL = f'https://gnews.io/api/v4/top-headlines?apikey={os.getenv("GNEW
 
 
 class GNews:
-    def __init__(self, user_chat_id: int, category: str):
+    def __init__(self, user_chat_id: int, category: str = None, q: str = None):
         self.user = User.query.get(user_chat_id)
-        self.category = category
-        self.g_news_url = f'{GNEWS_BASE_URL}&category={self.category}&country={self.user.settings.country}' \
-                          f'&lang={self.user.settings.language_code}'
+        if category:
+            self.g_news_url = f'{GNEWS_BASE_URL}&category={category}&country={self.user.settings.country}' \
+                              f'&lang={self.user.settings.language_code}'
+        elif q:
+            self.g_news_url = f'{GNEWS_BASE_URL}&q={q}&country={self.user.settings.country}' \
+                              f'&lang={self.user.settings.language_code}'
 
-    def get_headlines(self):
+    def get_news(self):
         old_articles = Article.query.filter_by(user_chat_id=self.user.chat_id).all()
         for article in old_articles:
             db.session.delete(article)
         db.session.commit()
         res = requests.get(url=self.g_news_url)
         data = res.json()
-        pprint(data)
         articles = data.get('articles')
+        pprint(articles)
         news = []
         for art in articles:
             new = Article(
                 user_chat_id=self.user.chat_id,
                 title=art.get('title'),
                 description=art.get('description'),
-                image_url= art.get('image'),
+                image_url=art.get('image'),
                 article_url=art.get('url')
             )
             news.append(new)
         db.session.bulk_save_objects(news)
         db.session.commit()
+
 
 
